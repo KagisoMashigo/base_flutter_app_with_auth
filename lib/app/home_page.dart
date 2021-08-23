97a85fcb6648cfe513d8173c5e7d11a9b9e76297
Iso Mashigo <kagiso.k.mashigo@gmail.com>
2021-08-23 14:59:00 -0400

    inital commit

---
+++ lib/app/home_page.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:time_tracker_flutter_course/common_widgets/show_alert_dialog.dart';
+import 'package:time_tracker_flutter_course/services/auth.dart';
+
+class HomePage extends StatelessWidget {
+
+
+  Future<void> _signOut(BuildContext context) async {
+  final auth = Provider.of<AuthBase>(context, listen: false);
+    try {
+      await auth.signOut();
+    } catch (e) {
+      print(e.toString());
+    }
+  }
+
+  Future<void> _confirmSignOut(BuildContext context) async {
+    final confirmSignOut = await showAlertDialog(
+      context,
+      title: 'Logout',
+      content: 'Are you sure you want to log out?',
+      defaultActionText: 'Logout',
+      cancelActionText: 'Cancel',
+    );
+    if (confirmSignOut == true) {
+      _signOut(context);
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+      title: Text('Home Page'),
+      actions: <Widget>[
+        FlatButton(
+          onPressed: () => _confirmSignOut(context),
+          child: Text(
+            'Logout',
+            style: TextStyle(
+              fontSize: 18.0,
+              color: Colors.white,
+            ),
+          ),
+        )
+      ],
+    ));
+  }
+}
